4a1beffd215a24b27bfa6857c0debc1d895d4a90
Sean Tierney <stierney0505@gmail.com>
2023-10-07 13:32:19 -0500

    Sean's work branch (#3)
    * Add stuff
    
    * We are Frauds
    
    * Final Commits before presentation
    
    * Touch Ups for HTML
    
    * Compatible with main?

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const path = require('path');
-// const test2Routes = require('./router/test2.js').test2Routes; //Gets the additional routes from the test2.js file
+const test2Routes = require('./router/test2.js').test2Routes; //Gets the additional routes from the test2.js file
+const login = require('./router/login.js').login;
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const app = express();
-const PORT = 3000; //Currently the port is hardcoded at 3000, can be changed if we decide to utilize a .env file
-const corsOptions = {
-    origin: 'http://localhost:5174',
-}
-
-app.use(cors(corsOptions));
+const PORT = process.env.PORT || 5000; //Currently the port is hardcoded at 3000, can be changed if we decide to utilize a .env file
+  
 
 app.listen(PORT, (error) =>{
     if(!error)
@@ -19,25 +18,31 @@ app.listen(PORT, (error) =>{
     }
 );
 
-//Adam - test route
-app.get('/api/data', (req, res) => {
-    const data = {
-        message: 'This is the data from the API route.',
-        timestamp: Date.now(),
-    }
-    res.json(data);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse application/json
+app.use(bodyParser.json());
 
 //Sends the home page 
 app.get('/', (req, res) => {
-    res.send("API is working properly");
-})
+    res.sendFile(path.join(__dirname, '/views/test-homepage.html'));
 
 /*This provides all of files from the public directory from the server, i.e. http://localhost:port/<FILE.NAME> and these files should be files like css styles, 
 js functions, and images. Anything that should be publically accessed by the frontend html */
 app.use(express.static('public'));
 
+
+//This provides node_modules files under the /static directory, for files such as bootstrap if we decide to sent them from our server instead of accessing them from the web
+app.use('/static', express.static(path.join(__dirname, 'node_modules'))); 
+
+/*activates the routes from the test2.js file under directory /test2. So all routes defined in test2.js are accessed by -> <baseurl> + /test2/ + <test2.js route> 
+For example the two routes in test2.js can be accessed by url http://127.0.0.22:3000/test2 and http://127.0.0.22:3000/test2/helloworld*/
+app.use('/test2', test2Routes); 
+
+app.use('/login', login);
+
 //This is the 404 Route. THIS MUST REMAIN LAST IT CATCHES ALL OTHER GET REQUESTS 
 app.get('*', function(req, res){
-    res.status(404).send("404 error, page not found");
+    res.status(404).sendFile(path.join(__dirname, 'views/404.html')) //send 404 error and file
 });
+
