36eeda62b315dbc016053a81af962cb0ed0f6492
lukebushur <luke7ajb@gmail.com>
2023-10-13 03:24:08 -0400

    Fixed server for working without mongodb database

--- server/router/login.js
+++ server/router/login.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 const genToken = require('../middleware/authentication.js').generateAccess;
 const bodyParser = require('body-parser');
-const userSchema = require('../models/user.js');
+// const userSchema = require('../models/user.js');
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 
- mongoose.connect(`${process.env.DB_PROTOCOL}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?${process.env.DB_PARAMS}`, {})
-    .then(async () => {
-        console.log("Connected to ATLAS");
-    }).catch((err) => {
-        console.log(err);
-    });
+//  mongoose.connect(`${process.env.DB_PROTOCOL}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?${process.env.DB_PARAMS}`, {})
+//     .then(async () => {
+//         console.log("Connected to ATLAS");
+//     }).catch((err) => {
+//         console.log(err);
+//     });
 
-process.on('SIGINT', () => {
-    mongoose.connection.close();
-    console.log('Mongoose disconnected on app termination');
-    process.exit(0);
-});
+// process.on('SIGINT', () => {
+//     mongoose.connection.close();
+//     console.log('Mongoose disconnected on app termination');
+//     process.exit(0);
+// });
 
 // Parse application/json
 router.use(bodyParser.json());
@@ -28,11 +28,11 @@ router.post('/', async (req, res) => {
     const user = req.body.username;
     const pass = req.body.password;
 
-    const userRecord = await userSchema.findOne({email:"tierst01@pfw.edu"});
-    let testPass = (userRecord.password);
+    // const userRecord = await userSchema.findOne({ email: "tierst01@pfw.edu" });
+    // let testPass = (userRecord.password);
 
 
-    if (pass == testPass) {
+    if (/*pass == testPass &&*/ user === "admin" && pass === "1234") {
         const token = genToken(user); // Generate token
         res.json({ token: token }); // Send token as JSON response
     }
