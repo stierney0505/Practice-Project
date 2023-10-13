961f6603ef82f06b086c19e79b8b5574e123be20
lukebushur <luke7ajb@gmail.com>
2023-10-13 02:57:45 -0400

    Fixed server syntax errors

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const test2Routes = require('./router/test2.js').test2Routes; //Gets the additional routes from the test2.js file
 const login = require('./router/login.js').login;
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000; //Currently the port is hardcoded at 3000, can be changed if we decide to utilize a .env file
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 //Sends the home page 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/test-homepage.html'));
+})
 
 /*This provides all of files from the public directory from the server, i.e. http://localhost:port/<FILE.NAME> and these files should be files like css styles, 
 js functions, and images. Anything that should be publically accessed by the frontend html */
@@ -45,4 +46,3 @@ app.use('/login', login);
 app.get('*', function(req, res){
     res.status(404).sendFile(path.join(__dirname, 'views/404.html')) //send 404 error and file
 });
-
